6a9fe228148b8925b8ae120a0491579e6711bf4c
ot07 <ot07neoki@gmail.com>
2023-01-27 23:51:14 +0900

    fix Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import {ChevronDownIcon, UserIcon} from "@heroicons/react/24/solid";
-import {BellIcon} from "@heroicons/react/24/outline";
+import {UserIcon} from "@heroicons/react/24/solid";
+import {BellIcon, ChevronDownIcon} from "@heroicons/react/24/outline";
 
 
 export const Header = () => {
@@ -21,9 +21,9 @@ export const Header = () => {
 
               <button className="flex justify-center items-center space-x-2">
                 <UserIcon className="bg-gray-500 rounded-full p-1 text-white w-7 h-7"/>
-                <div className="flex items-center">
-                  <span className="text-sm font-medium text-gray-600">Acme Inc.</span>
-                  <ChevronDownIcon className="translate-y-0.5 w-5 h-5 text-gray-500"/>
+                <div className="flex items-center space-x-1">
+                  <span className="text-sm font-medium text-gray-600">山田 太郎</span>
+                  <ChevronDownIcon className="translate-y-0.5 w-3 h-3 text-gray-500" strokeWidth={3} />
                 </div>
               </button>
             </div>
