913e6759f8f85ac2acde31087af993ad33994648
ot07 <ot07neoki@gmail.com>
2023-02-25 14:23:14 +0900

    add Table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,239 @@
+import { ChangeEvent, ReactNode, useState } from 'react'
+import {
+  createStyles,
+  Table as MantineTable,
+  ScrollArea,
+  UnstyledButton,
+  Group,
+  Text,
+  Center,
+  TextInput,
+  Checkbox,
+} from '@mantine/core'
+import { keys } from '@mantine/utils'
+import {
+  IconSelector,
+  IconChevronDown,
+  IconChevronUp,
+  IconSearch,
+} from '@tabler/icons'
+
+const useStyles = createStyles((theme) => ({
+  th: {
+    padding: '0 !important',
+  },
+
+  control: {
+    width: '100%',
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  icon: {
+    width: 21,
+    height: 21,
+    borderRadius: 21,
+  },
+
+  rowSelected: {
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.fn.rgba(theme.colors[theme.primaryColor][7], 0.2)
+        : theme.colors[theme.primaryColor][0],
+  },
+}))
+
+interface RowData {
+  id: string
+  name: string
+  email: string
+  company: string
+}
+
+interface TableProps {
+  data: RowData[]
+}
+
+interface ThProps {
+  children: ReactNode
+  reversed: boolean
+  sorted: boolean
+  onSort(): void
+}
+
+function Th({ children, reversed, sorted, onSort }: ThProps) {
+  const { classes } = useStyles()
+  const Icon = sorted
+    ? reversed
+      ? IconChevronUp
+      : IconChevronDown
+    : IconSelector
+  return (
+    <th className={classes.th}>
+      <UnstyledButton onClick={onSort} className={classes.control}>
+        <Group position="apart">
+          <Text weight={500} size="sm">
+            {children}
+          </Text>
+          <Center className={classes.icon}>
+            <Icon size={14} stroke={1.5} />
+          </Center>
+        </Group>
+      </UnstyledButton>
+    </th>
+  )
+}
+
+function filterData(data: RowData[], search: string) {
+  const query = search.toLowerCase().trim()
+  return data.filter((item) =>
+    keys(data[0]).some((key) => item[key].toLowerCase().includes(query))
+  )
+}
+
+function sortData(
+  data: RowData[],
+  payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
+) {
+  const { sortBy } = payload
+
+  if (!sortBy) {
+    return filterData(data, payload.search)
+  }
+
+  return filterData(
+    [...data].sort((a, b) => {
+      if (payload.reversed) {
+        return b[sortBy].localeCompare(a[sortBy])
+      }
+
+      return a[sortBy].localeCompare(b[sortBy])
+    }),
+    payload.search
+  )
+}
+
+export function Table({ data }: TableProps) {
+  const { classes, cx } = useStyles()
+  const [search, setSearch] = useState('')
+  const [sortedData, setSortedData] = useState(data)
+  const [sortBy, setSortBy] = useState<keyof RowData | null>(null)
+  const [reverseSortDirection, setReverseSortDirection] = useState(false)
+  const [selection, setSelection] = useState(['1'])
+
+  const setSorting = (field: keyof RowData) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false
+    setReverseSortDirection(reversed)
+    setSortBy(field)
+    setSortedData(sortData(data, { sortBy: field, reversed, search }))
+  }
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget
+    setSearch(value)
+    setSortedData(
+      sortData(data, { sortBy, reversed: reverseSortDirection, search: value })
+    )
+  }
+
+  const toggleRow = (id: string) =>
+    setSelection((current) =>
+      current.includes(id)
+        ? current.filter((item) => item !== id)
+        : [...current, id]
+    )
+  const toggleAll = () =>
+    setSelection((current) =>
+      current.length === data.length ? [] : data.map((item) => item.id)
+    )
+
+  const rows = sortedData.map((row) => {
+    const selected = selection.includes(row.id)
+    return (
+      <tr key={row.id} className={cx({ [classes.rowSelected]: selected })}>
+        <td>
+          <Checkbox
+            checked={selection.includes(row.id)}
+            onChange={() => toggleRow(row.id)}
+            transitionDuration={0}
+          />
+        </td>
+        <td>{row.name}</td>
+        <td>{row.email}</td>
+        <td>{row.company}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <ScrollArea>
+      <TextInput
+        placeholder="Search by any field"
+        mb="md"
+        icon={<IconSearch size={14} stroke={1.5} />}
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <MantineTable
+        horizontalSpacing="md"
+        verticalSpacing="xs"
+        sx={{ tableLayout: 'fixed', minWidth: 700 }}
+      >
+        <thead>
+          <tr>
+            <th style={{ width: 40 }}>
+              <Checkbox
+                onChange={toggleAll}
+                checked={selection.length === data.length}
+                indeterminate={
+                  selection.length > 0 && selection.length !== data.length
+                }
+                transitionDuration={0}
+              />
+            </th>
+            <Th
+              sorted={sortBy === 'name'}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting('name')}
+            >
+              名前
+            </Th>
+            <Th
+              sorted={sortBy === 'email'}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting('email')}
+            >
+              メールアドレス
+            </Th>
+            <Th
+              sorted={sortBy === 'company'}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting('company')}
+            >
+              会社
+            </Th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.length > 0 ? (
+            rows
+          ) : (
+            <tr>
+              <td colSpan={Object.keys(data[0]).length}>
+                <Text weight={500} align="center">
+                  Nothing found
+                </Text>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </MantineTable>
+    </ScrollArea>
+  )
+}
